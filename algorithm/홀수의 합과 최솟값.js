78196f5b44e37c1e7bdae056ce19d2d2d8b9d2be
김정진 <kimjungjin@gimjeongjin-ui-MacBookPro.local>
2021-05-11 00:49:35 +0900

    feat: 홀수의 합과 최솟값 문제풀이

---
+++ algorithm/홀수의 합과 최솟값.js
@@ -0,0 +1,21 @@
+function solution(arr){
+    let answer = [];
+    let sum = 0;
+    let min = Number.MAX_SAFE_INTEGER;
+    for (i = 0; i < arr.length; i++) {
+        if(arr[i] % 2){
+        sum += arr[i];
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+        }
+        
+    }
+    answer.push(sum);
+    answer.push(min);
+
+    return answer;
+}
+
+arr=[12, 77, 38, 41, 53, 92, 85];
+console.log(solution(arr));
