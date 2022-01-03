97c7a8b97de90fb9c836ecd61d9ef33de5ad6e25
kimjungjin <jungks9351@gmail.com>
2022-01-03 14:23:04 +0900

    feat : nextBtn, prevBtn pagination 페이지 번호 변경 구현

--- JavaScript/practice/movie-app/public/js/index.js
+++ JavaScript/practice/movie-app/public/js/index.js
@@ -7,13 +7,15 @@ window.onload = () => {
 
 const $moiveList = document.getElementById("movieList");
 const $nextBtn = document.getElementById("nextBtn");
+const $prevBtn = document.getElementById("prevBtn");
+const $pageNumbers = document.getElementById("pageNumbers");
 
 const url = new URLSearchParams(location.search);
 
+const lastPage = 20;
+
 const getPopularMovies = async () => {
   const pageNumber = url.get("page");
-
-  console.log(pageNumber);
 
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=b1f42a273f605f2b79d537a4c1929770&language=ko-KR&page=${pageNumber}`
@@ -38,6 +40,7 @@ const getPopularMovies = async () => {
 
     $moiveList.innerHTML += temp_html;
   });
+  changePageNumber(pageNumber);
 };
 
 // pagination
@@ -45,7 +48,35 @@ const getPopularMovies = async () => {
 $nextBtn.onclick = () => {
   const pageNumber = url.get("page") ? +url.get("page") : 1;
 
+  if (pageNumber >= lastPage) return;
   location.assign(`?page=${pageNumber + 1}`);
 
   getPopularMovies();
+};
+
+$prevBtn.onclick = () => {
+  const pageNumber = url.get("page") ? +url.get("page") : 1;
+
+  if (pageNumber <= 1) return;
+
+  location.assign(`?page=${pageNumber - 1}`);
+
+  getPopularMovies();
+};
+
+//page number 그리기
+
+const changePageNumber = (pageNumber) => {
+  $pageNumbers.innerHTML = "";
+  pageNumber = pageNumber === null ? 1 : +pageNumber;
+  if (pageNumber < 16) {
+    for (let i = pageNumber; i < pageNumber + 5; i++) {
+      console.log(i);
+      $pageNumbers.innerHTML += `<span class="page-number">${i}</span>`;
+    }
+  } else {
+    for (let j = pageNumber; j <= lastPage; j++) {
+      $pageNumbers.innerHTML += `<span class="page-number">${j}</span>`;
+    }
+  }
 };
